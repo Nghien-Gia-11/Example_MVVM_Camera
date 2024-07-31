2945afd96115fb31899674a91547bc38faa3ed15
Phung Gia Nghien <phungnghien03@gmail.com>
2024-07-31 15:13:09 +0700

    add commend

--- app/src/main/java/com/example/example_mvvm_camera/MlKitAnalyzer.kt
+++ app/src/main/java/com/example/example_mvvm_camera/MlKitAnalyzer.kt
@@ -8,34 +8,35 @@ import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 
 @OptIn(ExperimentalGetImage::class)
-fun scanQR(images: ImageProxy, qrViewModel: QrViewModel){
-
+fun scanQR(images: ImageProxy, qrViewModel: QrViewModel) {
+    // lấy ảnh từ ImageProxy
     val mediaImage = images.image
     if (mediaImage != null) {
+        // lấy ra đối tượng để phân tích mã vạch
         val scanner = BarcodeScanning.getClient()
+        // lấy ra ảnh để Ml Kit phân tích
         val inputImage =
             InputImage.fromMediaImage(mediaImage, images.imageInfo.rotationDegrees)
+        // phân tích mã vạch
         scanner.process(inputImage)
+            // phân tích thành công
             .addOnSuccessListener { barcodes ->
                 for (barcode in barcodes) {
-                    val rawValue = barcode.rawValue
-                    Log.d("MlKitAnalyzer", "QR Code detected: $rawValue")
                     barcode.rawValue?.let { qrCode ->
                         qrViewModel.setResult(qrCode)
                     }
-
                 }
             }
+            // phân tích thất bại
             .addOnFailureListener {
                 Log.e("MlKitAnalyzer", "Barcode scanning failed", it)
             }
+            // phân tích hoàn thành
             .addOnCompleteListener {
                 images.close()
             }
     } else {
         images.close()
     }
-
-
 }
 
